4b99b5615f03f7b6909ba83be05ce09c401c0c8f
Jonathan Beckman <jonathantbeckman@gmail.com>
2021-03-23 20:54:08 -0400

    Moved freqtrade into src module. Updated code to use config more consistently

--- src/config/const.py
+++ src/config/const.py
@@ -1,11 +1,11 @@
-# This file should be as basic as possible, with little to no dependencies
+# This file should be as basic as possible, with minimal dependencies. All paths should be strings where path logic is
+# handled by the caller
 
 MODEL_NAME = "giia"
 MODEL_VERSION = "0.3.3"
 MODEL_ID = f"{MODEL_NAME}-{MODEL_VERSION}"
 
-DATASET = "../freqtrade/user_data/data/binance/ETH_BTC-5m.json"
-SRC_DATASET_DIR = "../freqtrade/user_data/data/binance"
+FREQTRADE_USER_DATA_DIR = "freqtrade/user_data"
 SM_ROLE = 'arn:aws:iam::941048668662:role/service-role/AmazonSageMaker-ExecutionRole-20191206T145896'
 
 TRAIN_DATASET_FILENAME = "train.csv"
